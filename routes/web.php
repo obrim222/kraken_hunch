7867f640274e8b63ac53b60100e63b2ed641ac2a
Unicornflavour <tom-weniger@hotmail.com>
2021-12-13 17:16:20 +0100

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+use App\Http\Controllers\BlogController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\MailerController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -29,10 +34,14 @@ Route::get('/coins', function () {
 });
 
 
-Route::get('/blogs', function () {
-    return view('blogs');
-});
+Route::get('/blogs', [BlogController::class, 'viewBlogpost'])->name('blogs');
+Route::post('/blogs', [BlogController::class, 'addComment']);
 
+// Test Route
+Route::get('/post', [BlogController::class, 'viewBlogpost']);
+Route::post('/post', [BlogController::class, 'addComment']);
+
+//Route::get('/blogs', [BlogController::class, 'show']);
 
 
 Route::get('/hunch', function () {
@@ -48,6 +57,8 @@ Route::get('/about', function () {
     return view('about');
 });
 
+Auth::routes(['verify' => true]);
+
 Route::get('/login', [LoginController::class, 'login'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 
@@ -59,6 +70,17 @@ Route::get("email", [MailerController::class, "email"])->name("email");
 
 Route::post("send-email", [MailerController::class, "composeEmail"])->name("send-email");
 
+Route::get('/verify-email', [EmailVerificationPromptController::class, '__invoke'])
+    ->middleware('auth')
+    ->name('verification.notice');
+
+Route::get('/verify-email/{id}/{hash}', [VerifyEmailController::class, '__invoke'])
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
 
 //Test
 Route::get('/logout', [LogoutController::class, 'logout'])->name('logout');
