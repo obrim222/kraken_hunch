d896707418aef94034f113999ac1a85751827521
Tom <tom-weniger@hotmail.com>
2021-12-14 14:13:18 +0100

    Blogs add Comments

--- resources/views/blogs.blade.php
+++ resources/views/blogs.blade.php
@@ -5,53 +5,117 @@
 @section('content')
 
 
-    <section class="articles">
-        <div class="article__content container container--pall">
-            <h2>Latest Articles</h2>
+<section class="articles">
+    <div class="article__content container container--pall">
+        <h2>Latest Articles</h2>
 
-            <div class="article__grid">
-                <a href="#" class="article__item">
-                    <div class="article__image" style="background-image: url('./images/bitcoin.jpg')"></div>
-                    <div class="article__text">
-                        <div class="article__author">By Kasia Kolo</div>
-                        <div class="article__title">
-                            Bitcoin is touted as the new digital gold by Michael Saylor. Is
-                            he right?
+        <div class="article__grid">
+            <a href="#" class="article__item">
+                <div class="article__image" style="background-image: url('./images/bitcoin.jpg')"></div>
+                <div class="article__text">
+                    <div class="article__author">By Kasia Kolo</div>
+                    <div class="article__title">
+                        Bitcoin is touted as the new digital gold by Michael Saylor. Is
+                        he right?
+                    </div>
+                    <div class="article__description">
+                        Michael Saylor is a brave man. Having recently taken out a bank
+                        loan to buy 7000 bitcoin....
+                    </div>
+                    <br>
+                    <form action="{{ route('blogs') }}" method="post">
+                        @csrf
+
+                        <textarea name="blog" cols="15" rows="4"></textarea>
+
+                        @error('body')
+                        <div>
+                            {{ $message }}
                         </div>
-                        <div class="article__description">
-                            Michael Saylor is a brave man. Having recently taken out a bank
-                            loan to buy 7000 bitcoin....
+                        @enderror
+
+
+                        <div>
+                            <button type="submit">Comment</button>
                         </div>
-                    </div>
-                </a>
+                </div>
+            </a>
 
-                <a href="#" class="article__item">
-                    <div class="article__image" style="background-image: url('./images/ethereum.jpg')"></div>
-                    <div class="article__text">
-                        <div class="article__author">By Tom Weniger</div>
-                        <div class="article__title">Ethereum...</div>
-                        <div class="article__description">Our .......</div>
-                    </div>
-                </a>
+            <a href="#" class="article__item">
+                <div class="article__image" style="background-image: url('./images/ethereum.jpg')"></div>
+                <div class="article__text">
+                    <div class="article__author">By Tom Weniger</div>
+                    <div class="article__title">Ethereum...</div>
+                    <div class="article__description">Our .......</div>
+                </div>
+                <br>
+                <form action="{{ route('blogs') }}" method="post">
+                    @csrf
 
-                <a href="#" class="article__item">
-                    <div class="article__image" style="background-image: url('./images/solana.jpg')"></div>
-                    <div class="article__text">
-                        <div class="article__author">By Michael O Brien</div>
-                        <div class="article__title">.....</div>
-                        <div class="article__description">.....</div>
-                    </div>
-                </a>
+                    <textarea name="blog" cols="15" rows="4"></textarea>
 
-                <a href="#" class="article__item">
-                    <div class="article__image" style="background-image: url('./images/cardano.png')"></div>
-                    <div class="article__text">
-                        <div class="article__author">By Kasia Kolo</div>
-                        <div class="article__title">.....</div>
-                        <div class="article__description">.....</div>
+                    @error('body')
+                    <div>
+                        {{ $message }}
                     </div>
-                </a>
-            </div>
+                    @enderror
+
+
+                    <div>
+                        <button type="submit">Comment</button>
+                    </div>
+            </a>
+
+            <a href="#" class="article__item">
+                <div class="article__image" style="background-image: url('./images/solana.jpg')"></div>
+                <div class="article__text">
+                    <div class="article__author">By Michael O Brien</div>
+                    <div class="article__title">.....</div>
+                    <div class="article__description">.....</div>
+                </div>
+                <br>
+                <form action="{{ route('blogs') }}" method="post">
+                    @csrf
+
+                    <textarea name="blog" cols="15" rows="4"></textarea>
+
+                    @error('body')
+                    <div>
+                        {{ $message }}
+                    </div>
+                    @enderror
+
+
+                    <div>
+                        <button type="submit">Comment</button>
+                    </div>
+            </a>
+
+            <a href="#" class="article__item">
+                <div class="article__image" style="background-image: url('./images/cardano.png')"></div>
+                <div class="article__text">
+                    <div class="article__author">By Kasia Kolo</div>
+                    <div class="article__title">.....</div>
+                    <div class="article__description">.....</div>
+                </div>
+                <br>
+                <form action="{{ route('blogs') }}" method="post">
+                    @csrf
+
+                    <textarea name="blog" cols="15" rows="4"></textarea>
+
+                    @error('body')
+                    <div>
+                        {{ $message }}
+                    </div>
+                    @enderror
+
+
+                    <div>
+                        <button type="submit">Comment</button>
+                    </div>
+            </a>
         </div>
-    </section>
-    @endsection
+    </div>
+</section>
+@endsection
